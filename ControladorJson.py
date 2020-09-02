6c502c3f0839a1cef6bdd7c9ded34c0f72293bed
EduardoIxen <tomas-1539@hotmail.com>
2020-09-02 03:36:56 -0600

    Funcionalidad minimo,maximoo,suma, cuenta; agregadas

--- ControladorJson.py
+++ ControladorJson.py
@@ -1,40 +1,117 @@
 import json
+import re
+import  operator
 from Registro import *
 
 class ControladorJson:
     def cargarJson(self,ruta):
-        print('///////////////////////CARGANDO ARCHIVO JSON///////////////////////')
         with open(ruta) as contenido:
             productos = json.load(contenido)
             for producto in productos:
-                #print(producto)
                 Registro.registros.append(producto)
             #print(f'Tipo de archivos cargados: {type(producto)}')
 
     def seleccionar(self, atributos, llave):
-        print(atributos)
         if str(atributos) == "*":
             separarLlave = llave.split(sep='=', maxsplit=1)
-            print(separarLlave[0])
-            print(separarLlave[1])
-            for registro in Registro.registros:
-                comp = str(registro.get(separarLlave[0].lower().strip()))
-                if comp == str(separarLlave[1].strip()):
-                    print(registro)
+            #print(separarLlave[0])
+            #print(separarLlave[1])
+            if str(re.search("nombre", str(separarLlave[0]))) != "None":
+                nombreBuscar2 = str(separarLlave[1].replace("\"", ""))
+                nombreBuscar = str(separarLlave[1].replace("\"", "\'"))
+                if re.search('^\'.*\'$', nombreBuscar):
+                    for registro in Registro.registros:
+                        comp = str(registro.get(separarLlave[0].strip()))
+                        if comp == nombreBuscar2:
+                            print(registro)
+                    print("////////////////////////////////////////////////////////////")
+                else:
+                    print("El nombre no esta entre comillas")
+            else:
+                for registro in Registro.registros:
+                    comp = str(registro.get(separarLlave[0].lower().strip()))
+                    if comp == str(separarLlave[1].strip()):
+                        print(registro)
+                print("////////////////////////////////////////////////////////////")
         else:
             atributosSep = atributos.split(sep=',')
-            print("entro",atributos)
             separarLlave = llave.split(sep='=', maxsplit=1)
-            print(separarLlave[0])
-            print(separarLlave[1])
+            #print(separarLlave[0])
+            #print(separarLlave[1])
+            #print(re.search("nombre", str(separarLlave[0])))
+            #print(type(re.search("nombre", str(separarLlave[0]))))
+            if str(re.search("nombre", str(separarLlave[0]))) != "None":
+                nombreBuscar = str(separarLlave[1].replace("\"","\'"))
+                if re.search('^\'.*\'$',nombreBuscar):
+                    nombreBuscar2 =str(separarLlave[1].replace("\"",""))
+                    for registro in Registro.registros:
+                        comp = str(registro.get(separarLlave[0].strip()))
+                        #print("lo que vamos a comp",comp)
+                        if comp == nombreBuscar2:
+                            for atributo in atributosSep:
+                                try:
+                                    print(f"{atributo.lower().strip()}: ", registro[f'{atributo.lower().strip()}'])
+                                except KeyError as e:
+                                    print(f"El atributo {atributo.lower().strip()} no exixte")
+                            print("////////////////////////////////////////////////////////////")
+                else:
+                    print("El nombre no esta entre comillas.")
+            else:
+                for registro in Registro.registros:
+                    comp = str(registro.get(separarLlave[0].lower().strip()))
+                    if comp == str(separarLlave[1].strip()):
+                        for atributo in atributosSep:
+                            try:
+                                print(f"{atributo.lower().strip()}: ", registro[f'{atributo.lower().strip()}'])
+                            except KeyError as e:
+                                print(f"El atributo {atributo.lower().strip()} no exixte")
+                            #print(atributo)
+                        print("////////////////////////////////////////////////////////////")
+
+    def maximo(self,atributo):
+        listaEdad = []
+        listaPromedio = []
+        if atributo == 'edad':
             for registro in Registro.registros:
-                comp = str(registro.get(separarLlave[0].lower().strip()))
-                if comp == str(separarLlave[1].strip()):
-                    for atributo in atributosSep:
-                        try:
-                            print(f"{atributo.lower()}: ", registro[f'{atributo.lower()}'])
-                            print(f"{atributo.lower()}: ", registro['pppp'])
-                        except KeyError as e:
-                            print(f"El atributo {atributo.lower()} no exixte")
-                            break
-                        #print(atributo)
+                edad = registro.get('edad')
+                listaEdad.append(edad)
+            print("La edad maxima es de", max(listaEdad))
+        elif atributo == 'promedio':
+            for registro in Registro.registros:
+                promedio = registro.get('promedio')
+                listaPromedio.append(promedio)
+            print("El promedio maximo es de",max(listaPromedio))
+
+    def minimo(self,atributo):
+        listaEdad = []
+        listaPromedio = []
+        if atributo == 'edad':
+            for registro in Registro.registros:
+                edad = registro.get('edad')
+                listaEdad.append(edad)
+            print("La edad minima es de", min(listaEdad))
+        elif atributo == 'promedio':
+            for registro in Registro.registros:
+                promedio = registro.get('promedio')
+                listaPromedio.append(promedio)
+            print("El promedio minimo es de", min(listaPromedio))
+
+    def suma(self,atributo):
+        listaEdad = []
+        listaPromedio = []
+        if atributo == 'edad':
+            for registro in Registro.registros:
+                edad = registro.get('edad')
+                listaEdad.append(edad)
+            print("La suma de edades es:", sum(listaEdad))
+        elif atributo == 'promedio':
+            for registro in Registro.registros:
+                promedio = registro.get('promedio')
+                listaPromedio.append(promedio)
+            print("La suma de promedios es:", sum(listaPromedio))
+
+    def cuenta(self):
+        total = 0
+        for numRegistro in Registro.registros:
+            total = total+1
+        print("El numero total de cuentas registradas es: ",total)
--- main.py
+++ main.py
@@ -1,45 +1,67 @@
+import re
 import os.path as path
 from ControladorJson import *
 
 opcion = input("Ingrese el comando\n")
-opcion=opcion.upper()
+#opcion=opcion.upper()
 while True:
-    print(opcion)
     separado = opcion.split(maxsplit=1)
-    print(len(separado))
-    print(separado[0])
+    #print(len(separado))
+    #print(separado[0])
     # print(separado[1])
-    if separado[0] == 'CARGAR':
+    if separado[0].upper() == 'CARGAR':   #quitar el .upper
         listaDeArchivos = separado[1].split(sep=',')
         for archivos in listaDeArchivos:
-            print(archivos.strip())
+            #print(archivos.strip())
             if path.exists(f'entradas/{archivos.strip()}.json'):
-                print("Si existe")
                 controladorJson = ControladorJson()
                 controladorJson.cargarJson(f'entradas/{archivos.strip()}.json')
-            elif path.exists(f'entradas/{archivos.strip().lower()}.json'):
-                print("Si existe2")
+                print(f"{archivos.strip()} cargado correctamente!")
+                print("////////////////////////////////////////////////////////////")
+            elif path.exists(f'entradas/{archivos.strip().upper()}.json'): #cambiar por .lower
                 controladorJson = ControladorJson()
-                controladorJson.cargarJson(f'entradas/{archivos.strip().lower()}.json')
+                controladorJson.cargarJson(f'entradas/{archivos.strip().lower()}.json') #cambiar por .lower
+                print(f"{archivos.strip()} cargado correctamente!")
+                print("////////////////////////////////////////////////////////////")
             else:
-                print("No existe el archivo")
-    elif separado[0] == 'SELECCIONAR':
-        listaDeAtributos = separado[1].split(sep='DONDE', maxsplit=1)  # crea dos arreglos (atributos y valor ="
+                print("No existe el archivo ", archivos.strip())
+                print("////////////////////////////////////////////////////////////")
+    elif separado[0].upper() == 'SELECCIONAR':
+        listaDeAtributos = re.split('donde',str(separado[1]),flags=re.IGNORECASE)
         if listaDeAtributos[0].strip() == '*':
-            print("Listar todos los atributos")
             controladorJson = ControladorJson()
             controladorJson.seleccionar(listaDeAtributos[0].strip(), listaDeAtributos[1].strip())
         else:
-            print("Separar atributos")
+            try:
+                controladorJson = ControladorJson()
+                controladorJson.seleccionar(listaDeAtributos[0].strip(), listaDeAtributos[1].strip())
+            except IndexError as e:
+                print("Formato de entrada no valido")
+    elif separado[0].upper() =='MAXIMO':
+        if re.search("edad", separado[1], flags=re.IGNORECASE):
             controladorJson = ControladorJson()
-            controladorJson.seleccionar(listaDeAtributos[0].strip(), listaDeAtributos[1].strip())
+            controladorJson.maximo(separado[1].lower().strip())
+        elif re.search("promedio", separado[1], flags=re.IGNORECASE):
+            controladorJson = ControladorJson()
+            controladorJson.maximo(separado[1].lower().strip())
+    elif separado[0].upper() == 'MINIMO':
+        if re.search("edad", separado[1], flags=re.IGNORECASE):
+            controladorJson = ControladorJson()
+            controladorJson.minimo(separado[1].lower().strip())
+        elif re.search("promedio", separado[1], flags=re.IGNORECASE):
+            controladorJson = ControladorJson()
+            controladorJson.minimo(separado[1].lower().strip())
+    elif separado[0].upper() == 'SUMA':
+        if re.search("edad", separado[1], flags=re.IGNORECASE):
+            controladorJson = ControladorJson()
+            controladorJson.suma(separado[1].lower().strip())
+        elif re.search("promedio", separado[1], flags=re.IGNORECASE):
+            controladorJson = ControladorJson()
+            controladorJson.suma(separado[1].lower().strip())
+    elif separado[0].upper() == 'CUENTA':
+        controladorJson = ControladorJson()
+        controladorJson.cuenta()
     else:
         print('El comando no existe')
 
     opcion = input("Ingrese el comando\n")
-    opcion = opcion.upper()
-
-'''print("Imprimiendo todos los datos:")
-for data in Registro.registros:
-    print(data)
-print(f'Tipo de archivo cargado: {type(data)}')'''
